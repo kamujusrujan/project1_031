dbdf4b9f2464a665748ac26935017a0e5ca0b6d9
kamujusrujan <kamujusrujan01@gmail.com>
2020-04-15 19:48:41 +0530

    done with login page

---
+++ run.bat
@@ -0,0 +1,4 @@
+Set FLASK_APP=application.py 
+Set FLASK_DEBUG=1  
+Set DATABASE_URL ="postgres://yaprivoxszquqh:15286f549938a24481a41260c8284a1d5bc9ef44a4cba4cb036a80d270464db2@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d3qsd3bj25t98i"
+flask run
