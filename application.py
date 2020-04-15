58420e583123056318150c12bac024c012bb9aa2
kamujusrujan <kamujusrujan01@gmail.com>
2020-04-16 01:57:58 +0530

    completed login,register and admin page

--- application.py
+++ application.py
@@ -1,41 +1,82 @@
 import os
+from functools import wraps
 
-from flask import Flask, session,render_template,request
-from flask_session import Session
+from flask import Flask, session,render_template,request,url_for,redirect
+import random 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from database import *
 
 app = Flask(__name__)
-
-# Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
-
-# Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
+app.secret_key = 'srujan_123'
 
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:srujan@localhost:5432/test"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'log' not in session :
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 
-@app.route("/")	
+@app.route("/") 
 def index():
-    return render_template('mainpage.html')
+    db.create_all()
+    if 'log' in session :
+        return render_template('mainpage.html',name = session['name'])
+    return render_template('mainpage.html', name = '')
    
-
 
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
-	if (request.method == "GET"):
-		return render_template('loginpage.html')
+    if(request.method == 'GET'):
+        return render_template('loginpage.html')
+    mail,password = (request.form.get('mailid'), request.form.get('password'))
+    u = User.query.filter_by(mail = mail).first()
+    if u.password == password:
+        session['log'] = True
+        session['name'] = u.name
+        return render_template('success.html',mail=mail)
+    else:
+        return render_template('loginpage.html')
 
-	mail,password = (request.form.get('mailid'), request.form.get('password'))
-	return render_template('success.html',mail=mail)
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return 'dashboard'
+
+
+
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    if (request.method == "GET"):
+        return render_template('registerpage.html')
+
+    mail,password = (request.form.get('mailid'), request.form.get('password'))
+    db.session.add(User(mail = mail, name = mail.split('@')[0],password = password))
+    db.session.commit()
+    session['log'] = True
+    return render_template('success.html',mail=mail)
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('dashboard'))
+
+
+
+@app.route('/admin')
+def admin():
+    userlist= User.query.order_by(User.created_data).all()
+
+    return render_template('admin.html',list = userlist)
---
+++ database.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+class User(db.Model):
+	__tablename__ = 'registeredUsers'
+	mail = db.Column(db.String,primary_key = True)
+	name = db.Column(db.String)
+	password = db.Column(db.String,primary_key = True)
+	created_data  = db.Column(db.DateTime, default = datetime.datetime.utcnow)
